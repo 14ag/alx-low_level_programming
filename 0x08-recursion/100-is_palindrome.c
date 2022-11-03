1354e49f2da7732b9b5c238882192cc977beb244
14ag <muriukipn@gmail.com>
2022-11-04 00:58:50 +0300

    beta

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,39 +1,42 @@
-#include "main.h"
-
 /**
- * _myStrlen - gets string length
- * @p: string to be counted
- * Return: returns string length
- */
-char _myStrlen(char *p)
+ * _strlen_recursion - finds the length of a string
+ * @s: pointer to string
+ * Return: length of string
+ **/
+int _strlen_recursion(char *s)
 {
-	if (!*p)
+	if (*s == '\0')
 		return (0);
-	return (1 + _myStrlen(p + 1));
+	return (_strlen_recursion(s + 1) + 1);
 }
 /**
- * p1 - string iterator
- * @s: string paramsss
- * @l: length of string param
- * Return: returns string length
- */
-int p1(char *s, int l)
-{
-	if (l < 1)
-		return (1);
-	if (*s == *(s + l))
-		return (p1(s + 1, l - 2));
-	return (0);
-}
-/**
- * is_palindrome - checks if a string is palindrome
+ * Palindrome_Rsv - checks if two chars of a string are equal
  * @s: string to be checked
- * Return: 1 or 0 if palindrome or not palindrome respectively
- */
+ * @i: first index
+ * @j: last index
+ * Return: 1 if equal, else 0
+ **/
+int Palindrome_Rsv(char *s, int i, int j)
+{
+	if (i == j)
+		return (1);
+	if (i == j - 1)
+		return (s[i] == s[j]);
+	if (s[i] != s[j])
+		return (0);
+	return (Palindrome_Rsv(s, i + 1, j - 1));
+}
+/**
+ * is_palindrome - checks if a string is a palindrome
+ * @s: pointer to string
+ * Return: 1 if string is palindrome  else 0
+ **/
 int is_palindrome(char *s)
 {
 	int len;
 
-	len = _myStrlen(s);
-	return (p1(s, len - 1));
+	len = _strlen_recursion(s);
+	if (len == 0 || *s != s[len - 1])
+		return (0);
+	return (Palindrome_Rsv(s, 0, len - 1));
 }
